b92a817fa88ce20afb1ffadce935c22e1d905ee6
ryu2200 <dragonash2200@gmail.com>
2015-10-25 05:08:15 +0900

    a

---
+++ movieplot.py
@@ -0,0 +1,46 @@
+#coding:utf-8
+import sys #モジュール属性を取得するため
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+#読み込んだ値を格納する配列
+x=[0]*3
+y=[0]*3
+z=[0]*3
+#コマンドラインから引数を読み込む
+argvs=sys.argv
+argc=len(argvs)
+#デバッグプリント
+print u"引数は%sです"%argvs
+print u"個数は%sです"%argc
+if(argc!=2):
+	print "Usage:invalid"
+	quit()
+#argvs[0]はpyファイル
+f=open(argvs[1])
+i=0
+line=f.readline()
+while line:
+	x[i]=int(line[0])
+	y[i]=int(line[2])
+	#次の行を読み込む
+	line=f.readline()
+	i+=1
+f.close
+#読み込んだ値を元にアニメーションを描画する
+fig=plt.figure()
+# データを作成
+data= []
+for i in range(3):
+    im = plt.scatter(x[i], y[i])
+    data.append([im])
+# 動画保存の準備
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+# アニメーション作成
+#intervalでplot間隔を調整
+ani = animation.ArtistAnimation(fig, data, interval=10000, repeat_delay=1000)
+#動画として保存
+ani.save('im.mp4', writer=writer)
+# 表示
+plt.show()
--- rubykernel.rb
+++ rubykernel.rb
@@ -1,12 +1,15 @@
 #coding:utf-8
-#ruby -Ku
 require "kconv"
-
 def printHello
-	print("Hello¥n")
+	print("Hello\n")
 end
+stringarray=["山田","田中","藤原"]
+#配列の中身を出力する
+stringarray.each {|array|
+#macでは¥は環境依存文字のため改行はできない\で改行する必要がある
+print(Kconv.toutf8("氏名:"+array[0]+array[1]),"\n")
+}
 #kconv.toutf8で文字列をutf-8に変換する
 print(Kconv.toutf8("メソッドを呼び出す"))
 printHello
-print 
 
